7ef9ece95e7b21e85eb33db1dca09a0c14b7482a
Sneha <snehazacharia@outlook.com>
2019-01-08 15:57:24 -0500

    assign shift changes

--- tristatehcApp/src/main/webapp/tristatehc-app/src/app/assign-shifts/assign-shifts.component.ts
+++ tristatehcApp/src/main/webapp/tristatehc-app/src/app/assign-shifts/assign-shifts.component.ts
@@ -23,22 +23,20 @@ export class AssignShiftsComponent implements OnInit {
     titles:SelectItem[];
     shifts:SelectItem[];
     notifyNames:SelectItem[];
+    changeReasons:SelectItem[];
     userAvailabilities : AssignShift[] = [];
     
     assignedShift:AssignShift = new AssignShift();
     blocked : boolean;
     showTable : boolean;
-    maxRadius : number;
+   
     radius : number;
     filteredZipcode : any[] = [];
     selectedZipcode : any = "";
     facilities: any[] = [];
     selectedFacility: string="";
-    selectedShift7To3 :string[] = [];
-    selectedShift3To11 : string[] = [];
-    selectedShift11To7: string[] = [];
-    selectedShiftOther: string[] = [];
-    selectedTitle: string="All";
+    futureRadioButton :string[] = [];
+    selectedFuture: string="All";
     selectedEliminateBooked : string[] = [];
     selectedEliminateBanned : string[] = [];
     selectedEliminatePending : string[] = [];
@@ -46,9 +44,11 @@ export class AssignShiftsComponent implements OnInit {
     defaultDate: Date;
     shiftDateFrom : Date;
     shiftDateTo : Date;
-    msgs: string = ""; //city message
+    changeReason: string = ""; //city message
+   
     loggedInUserEmail : string ="";
     displayDialog :boolean =false;
+     displayNameChange : boolean =false;
     
     country: any;
 
@@ -76,6 +76,7 @@ export class AssignShiftsComponent implements OnInit {
           ];
 
       this.names=[{label:'Kurian', value:"Kurian"},{label:"Sneha", value:"Sneha"}];
+      this.changeReasons=[{label:'Select', value:"Select"},{label:'Person is not available', value:"Person is not available"},{label:"Person was wrongly allocated", value:"Person was wrongly allocated"}];
       this.titles=[{label:'CNA', value:"CNA"},{label:"RN", value:"RN"}];
       this.shifts=[{label:'7-3', value:"7-3"},{label:"5-11", value:"5-11"}];
       this.notifyNames=[{label:'Harinder', value:"Harinder"},{label:"Loyola", value:"Loyola"},{label:'Kurian', value:"Kurian"},{label:"Sneha", value:"Sneha"}];
@@ -153,6 +154,19 @@ export class AssignShiftsComponent implements OnInit {
         this.userAvailabilities.push(addedAssignShift);
         this.displayDialog=false;
         
+    }
+    
+    nameChanged(event:Event){
+    console.log("name changed--->"+event.value); 
+        this.displayNameChange=true;
+          
+    
+    }
+    
+    saveReason(){
+    console.log(this.changeReason);
+        this.displayNameChange=false;
+        this.changeReason="Select";  
     }
 
 }
